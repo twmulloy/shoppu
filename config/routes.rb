c477411c24025bd148860950ebfc85a4aed91a7a
Thomas Mulloy <twmulloy@gmail.com>
2017-04-29 16:25:37 -0700

    Spree devise into Alchemy

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  constraints subdomain: 'store' do
-    mount Spree::Core::Engine, at: '/'
-  end
-
   devise_scope :spree_user do
+    delete '/logout', to: 'spree/user_sessions#destroy'
+
     get '/login', to: 'spree/user_sessions#new'
     post '/login', to: 'spree/user_sessions#create'
-    delete '/logout', to: 'spree/user_sessions#destroy'
+    get '/logout', to: 'spree/user_sessions#destroy'
+    get '/signup', to: 'spree/user_registrations#new'
+    post '/signup', to: 'spree/user_registrations#create'
+    get '/password/recover', to: 'spree/user_passwords#new'
+    post '/password/recover', to: 'spree/user_passwords#create'
+    get '/password/change', to: 'spree/user_passwords#edit'
+    put '/password/change', to: 'spree/user_passwords#update'
+    get '/confirm', to: 'spree/user_confirmations#show' if Spree::Auth::Config[:confirmable]
+  end
+
+  constraints subdomain: 'store' do
+    mount Spree::Core::Engine, at: '/'
   end
 
   mount Alchemy::Engine, at: '/'
